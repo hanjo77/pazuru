bae330ca0e008cfa27bfab41e86f372cde0bd19d
hanjo77 <hanjo77@gmail.com>
2015-03-28 12:29:39 +0100

    Added local jQuery library

--- index.php
+++ index.php
@@ -9,7 +9,7 @@
 	}
 
 	</style>
-	<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+	<script src="js/jquery.min.js"></script>
 	<script type="text/javascript" src="js/pazuru.class.js"></script>
 </head>
 <body>
