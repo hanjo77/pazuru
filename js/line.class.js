d993eaceb636d7558621ddaea4e413a6581f110b
hanjo77 <hanjo77@gmail.com>
2015-03-29 03:35:13 +0200

    Added shades to line to recognize direction

--- js/line.class.js
+++ js/line.class.js
@@ -35,7 +35,7 @@ function Line(type, startX, startY, size, targetSize) {
 Line.prototype.draw = function(context) {
 
 	context.strokeStyle = '#000000';
-	context.lineWidth = config.lineWidth;
+	context.lineWidth = config.lineWidth*1.5;
 	context.beginPath();
 	if (this.targetSize > this.size) {
 
@@ -53,28 +53,57 @@ Line.prototype.draw = function(context) {
 			this.size = this.targetSize;
 		}
 	}
-	switch(this.type) {
+	var motion = [0, 0];
+	switch (this.type) {
 
 		case 1: // right
 			this.endX = this.startX+this.size;
 			this.endY = this.startY;
+			motion[0] = 1;
 			break;
 		case 2: // down
 			this.endX = this.startX;
 			this.endY = this.startY+this.size;
+			motion[1] = 1;
 			break;
 		case 3: // left
 			this.endX = this.startX-this.size;
 			this.endY = this.startY;
+			motion[0] = -1;
 			break;
 		case 4: // up
 			this.endX = this.startX;
 			this.endY = this.startY-this.size;
+			motion[1] = -1;
 			break;
 	}
 	context.moveTo(this.startX, this.startY);
 	context.lineTo(this.endX, this.endY);
 	context.stroke();
+
+	if (this.targetSize >= this.size) {
+
+		var shades = [
+			"#ffffff",
+			"#cccccc",
+			"#999999",
+			"#666666",
+			"#333333"
+		];
+		var len = ((this.size/2)/shades.length);
+		for (var i = shades.length-1; i >= 0; i--) {
+
+			if (i*len <= this.size) {
+
+				context.strokeStyle = shades[i];
+				context.lineWidth = config.lineWidth*1.5;
+				context.beginPath();
+				context.moveTo(this.startX, this.startY);
+				context.lineTo(this.startX+(i*len*motion[0]), this.startY+(i*len*motion[1]));
+				context.stroke();
+			}
+		}
+	}
 }
 
 Line.prototype.move = function() {
